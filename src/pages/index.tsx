e6233e534847809f3a9040f070e2ae0f46d84c1d
Tuan Hoang <tuanhoang5050@gmail.com>
2026-07-08 17:00:39 +0700

    vercel remove

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,15 +20,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+           
           </a>
         </div>
       </div>
